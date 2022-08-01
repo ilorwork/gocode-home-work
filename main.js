c5e43b1a5b82e283eaacca557b87e901e550cbd4
ilorwork <ilorwork64@gmail.com>
2022-08-01 14:06:06 +0300

    Solve 16ex

--- main.js
+++ main.js
@@ -1,73 +1,153 @@
-// 1
-// const createArray = (numOfCells) => {
-//   //const array = [numOfCells];
-//   const array = new Array(numOfCells);
-//   return array;
-// };
-
-// const numOfCells = Number(prompt("Enter the num of cells for the array"));
-// const array = createArray(numOfCells);
-// console.log(array);
-
-// // 2
-// const doubled = (array1, array2) => {
-//   for (let i = 0; i < array2.length; i++) {
-//     //debugger;
-//     array1[array1.length] = array2[i];
+// const powArray = (arr) => {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     newArr[i] = arr[i] ** 2;
 //   }
-//   return array1;
+//   return newArr;
 // };
 
-// let arr = [40, 100, 1, 5, 25, 10];
-// let arr2 = [6, 5, 4];
-// // a.push.apply(arr, arr2);
-// const mergedArr = doubled(arr, arr2);
-// console.log(mergedArr);
+// const numbersArr = [2, 3, 4];
+
+// console.log(powArray(numbersArr));
+
+// const powedArr = numbersArr.map((num) => {
+//   return num ** 2;
+// });
+// console.log(powedArr);
+
+// 2 - no need to answer - almog said.
 
 // // 3
-// const reverseArr = (array) => {
-//   let reversedArr = [];
-//   for (let i = 0; i < array.length; i++) {
-//     reversedArr[i] = array[array.length - (i + 1)];
+// const last = (arr, num) => {
+//   if (num <= 0 || typeof num === "undefined") {
+//     // Do not use pop, it will cut the end of the original array.
+//     const lastCellArr = [arr[arr.length - 1]];
+//     return lastCellArr;
+//   } else if (num > arr.length) {
+//     return arr;
 //   }
-//   return reversedArr;
+//   return arr.slice(arr.length - num, arr.length);
 // };
 
-// let arr = [40, 100, 1, 5, 25, 10];
-// const reversed = reverseArr(arr);
-// console.log(reversed);
+// const lastUsingLoop = (arr, num) => {
+//   if (num <= 0 || typeof num === "undefined") {
+//     // Do not use pop, it will cut the end of the original array.
+//     const lastCellArr = [arr[arr.length - 1]];
+//     return lastCellArr;
+//   } else if (num > arr.length) {
+//     return arr;
+//   }
+
+//   lastArr = [];
+//   let index = 0;
+//   for (let j = arr.length - num; j < arr.length; j++) {
+//     lastArr[index] = arr[j];
+//     index++;
+//   }
+//   return lastArr;
+// };
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 9];
+// console.log(last(numbers));
+// console.log(numbers);
+// console.log(lastUsingLoop(numbers, -1));
+// console.log(numbers);
 
 // // 4
-// const tenLengthArr = (firstValue) => {
-//   let array = [];
-//   for (let i = 0; i < 10; i++) {
-//     array[i] = firstValue + i;
-//   }
-//   return array;
+// const noCapital = (arr) => {
+//   return arr.filter((name) => {
+//     return name.toLowerCase() === name;
+//   });
 // };
 
-// const arr = tenLengthArr(Number(prompt("Enter the first value")));
-// console.log(arr);
+// const namesArr = ["Avi", "Dani", "shuki", "mOshe", "arik"];
+// console.log(noCapital(namesArr));
 
 // // 5
-// const createDog = (dogEyeColor, dogWeight, dogAge) => {
-//   dog = {
-//     eyeColor: dogEyeColor,
-//     weight: dogWeight,
-//     age: dogAge,
-//   };
-//   return dog;
+// const reverseStr = (arr) => {
+//   return arr.reverse();
 // };
 
-// const getDogs = () => {
-//   let dogsArr = [];
-//   for (let i = 0; i < 2; i++) {
-//     const dogEyeColor = prompt("Enter the dog's eye color");
-//     const dogWeight = Number(prompt("Enter the dog's weight"));
-//     const dogAge = Number(prompt("Enter the dog's age"));
-//     dogsArr[i] = createDog(dogEyeColor, dogWeight, dogAge);
+// const namesArr = ["Avi", "Dani", "shuki", "mOshe", "arik"];
+// console.log(reverseStr(namesArr));
+
+// // 5
+// const reverseStr = (str) => {
+//   const arr = str.split("");
+//   arr.reverse();
+//   const reversed = arr.join("");
+//   return reversed;
+// };
+
+// // const name = "Dani";
+// // console.log(reverseStr(name));
+
+// // 6
+// const findPolindroms = (arr) => {
+//   return arr.filter((word) => reverseStr(word) === word);
+// };
+
+// const wordsArr = ["aba", "goog", "shuki", "bgugb", "arik", "a", "rr", "fdsa"];
+// console.log(findPolindroms(wordsArr));
+
+// // 7
+// // const delAnimals = (animals, animalsToDelete) => {
+// //   return animals.filter(
+// //     (animal) => animalsToDelete.findIndex((animal) => animal) === -1
+// //   );
+// // };
+
+// const delAnimals = (animals, animalsToDelete) => {
+//   return animals.filter((animal) => animalsToDelete.indexOf(animal) === -1);
+// };
+
+// const animalsArr = ["lion", "camel", "fly", "butterfly", "zebra", "monky"];
+// const animalsToDelArr = ["fly", "butterfly"];
+// console.log(delAnimals(animalsArr, animalsToDelArr));
+
+// // 8
+// function getRandomArbitrary(min, max) {
+//   return Math.floor(Math.random() * (max - min) + min);
+// }
+// //console.log(getRandomArbitrary(1, 7));
+
+// const guessNumber = () => {
+//   const generatedNum = getRandomArbitrary(1, 7);
+//   console.log(generatedNum);
+//   const guesses = [];
+//   while (true) {
+//     let guessedNum = Number(prompt("Guess the num (between 1-6)"));
+//     if (guessedNum === generatedNum) {
+//       console.log("Correct!");
+//       console.log("Your wrong guesses were: " + guesses);
+//       break;
+//     }
+//     guesses.push(guessedNum);
 //   }
-//   return dogsArr;
 // };
 
-// console.log(getDogs());
+// guessNumber();
+
+// 9 - seemed like we need to use sort
+
+// 10
+// a:
+const a = [1, 2];
+let b = [];
+b = a;
+console.log(b);
+
+// b:
+const c = [1, 2];
+let d = c.map((o) => o);
+console.log(d);
+
+// c:
+const e = [1, 2];
+let f = [];
+
+for (let i = 0; i < e.length; i++) {
+  f[i] = e[i];
+}
+
+console.log(f);
