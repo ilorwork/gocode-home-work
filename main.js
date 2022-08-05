8ce1f35f6ceac02ed29831fc71cdad7a90ac2c06
ilorwork <ilorwork64@gmail.com>
2022-08-05 13:48:05 +0300

    Create the todo part

---
+++ main.js
@@ -0,0 +1,84 @@
+const addTodoInput = document.querySelector(".todo-input");
+const addTodoBtn = document.createElement("button");
+const pageListElement = document.querySelector(".page-list");
+
+addTodoBtn.className = "add-todo-item-btn";
+addTodoBtn.innerText = "add";
+
+addTodoInput.addEventListener("keyup", () => {
+  if (!!addTodoInput.value && addTodoInput.value !== "") {
+    addTodoInput.parentElement.appendChild(addTodoBtn);
+  } else if (
+    !addTodoInput.value &&
+    document.querySelector(".add-todo-item-btn")
+  ) {
+    addTodoBtn.parentElement.removeChild(addTodoBtn);
+  }
+});
+
+const todoArr = [];
+
+const todoListBtn = document.querySelector(".todo-list-btn");
+todoListBtn.addEventListener("click", () => {
+  reRenderToDoList(todoArr);
+});
+
+addTodoBtn.addEventListener("click", () => {
+  if (!addTodoInput.value) return;
+
+  const todoItem = { value: addTodoInput.value, isDone: false, id: Date.now() };
+  todoArr.push(todoItem);
+  //   pageListElement.innerHTML = "";
+
+  addTodoItem(todoArr[todoArr.length - 1]);
+
+  //   todoArr.forEach((item) => {});
+
+  addTodoInput.value = "";
+  addTodoBtn.parentElement.removeChild(addTodoBtn);
+});
+
+const reRenderToDoList = (todoList) => {
+  pageListElement.innerHTML = "";
+  todoList.forEach((item) => {
+    addTodoItem(item);
+  });
+};
+
+const addTodoItem = (todoItem) => {
+  const todoItemElement = document.createElement("div");
+  todoItemElement.className = "todo-item";
+
+  const itemBtnsContainer = document.createElement("div");
+  itemBtnsContainer.className = "item-btns-container";
+
+  const itemRemoveBtn = document.createElement("button");
+  itemRemoveBtn.className = "item-remove-btn";
+  itemRemoveBtn.innerText = "remove";
+
+  const itemDoneBtn = document.createElement("button");
+  itemDoneBtn.className = "item-done-btn";
+  itemDoneBtn.innerText = "done";
+
+  todoItemElement.innerText = todoItem.value;
+  itemBtnsContainer.appendChild(itemRemoveBtn);
+  itemBtnsContainer.appendChild(itemDoneBtn);
+  todoItemElement.appendChild(itemBtnsContainer);
+  pageListElement.appendChild(todoItemElement);
+
+  //   pageListElement.innerHTML += `<div class="todo-item">${todoArr[0].value}
+  //   <div class="item-btns-container">
+  //     <button class="item-remove-btn">remove</button>
+  //     <button class="item-done-btn">done</button>
+  //   </div>`;
+  addRemoveItemListener(itemRemoveBtn, todoItem.id, todoItemElement);
+};
+
+const addRemoveItemListener = (element, itemId /* , elementToDelete */) => {
+  element.addEventListener("click", () => {
+    const itemIndex = todoArr.findIndex((item) => item.id === itemId);
+    todoArr.splice(itemIndex, 1);
+    //elementToDelete.parentElement.removeChild(elementToDelete);
+    reRenderToDoList(todoArr);
+  });
+};
