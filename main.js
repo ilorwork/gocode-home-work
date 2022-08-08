1ac9bc65d51f34b4871fc881eb737c426bc8545b
ilorwork <ilorwork64@gmail.com>
2022-08-09 00:37:34 +0300

    1 version - placing the items using column-reverse

--- main.js
+++ main.js
@@ -5,15 +5,39 @@ const pageListElement = document.querySelector(".page-list");
 addTodoBtn.className = "add-todo-item-btn";
 addTodoBtn.innerText = "add";
 
-addTodoInput.addEventListener("keyup", () => {
-  if (!!addTodoInput.value && addTodoInput.value !== "") {
+addTodoInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    // https://stackoverflow.com/questions/67698860/javascript-call-addeventlistener-from-another-addeventlistener
+    // Another way is using addTodoBtn.click()
+    let event = new Event("click");
+    addTodoBtn.dispatchEvent(event);
+  }
+
+  if (addTodoInput.value) {
     addTodoInput.parentElement.appendChild(addTodoBtn);
   } else if (
     !addTodoInput.value &&
     document.querySelector(".add-todo-item-btn")
   ) {
-    addTodoBtn.parentElement.removeChild(addTodoBtn);
+    addTodoInput.parentElement.removeChild(addTodoBtn);
   }
+});
+
+addTodoBtn.addEventListener("click", () => {
+  if (!addTodoInput.value) return;
+
+  const todoItem = { value: addTodoInput.value, id: Date.now() };
+  todoArr.push(todoItem);
+
+  reRenderToDoList(todoArr);
+
+  addTodoInput.value = "";
+  // TODO: from unknown reason somtimes this line causes "Uncaught TypeError: Cannot read properties of null (reading 'removeChild')" exeption.
+  // possible workaround - using contitional access. Edit - doesnt helping!!
+  // Seemed to happened when pressing the Enter and another key at the same time.
+  //if (document.querySelector(".add-todo-item-btn")) return;
+
+  addTodoBtn.parentElement.removeChild(addTodoBtn);
 });
 
 let todoArr = [];
@@ -22,6 +46,38 @@ const todoListBtn = document.querySelector(".todo-list-btn");
 todoListBtn.addEventListener("click", () => {
   reRenderToDoList(todoArr);
 });
+
+const reRenderToDoList = (todoList) => {
+  pageListElement.innerHTML = "";
+  todoList.forEach((item) => {
+    renderTodoItem(item);
+  });
+};
+
+const renderTodoItem = (todoItem) => {
+  const todoItemElement = document.createElement("div");
+  todoItemElement.className = "todo-item";
+  todoItemElement.innerText = todoItem.value;
+
+  const itemBtnsContainer = document.createElement("div");
+  itemBtnsContainer.className = "item-btns-container";
+
+  const itemRemoveBtn = document.createElement("button");
+  itemRemoveBtn.className = "item-remove-btn";
+  itemRemoveBtn.innerText = "remove";
+
+  const itemDoneBtn = document.createElement("button");
+  itemDoneBtn.className = "item-done-btn";
+  itemDoneBtn.innerText = "done";
+
+  itemBtnsContainer.appendChild(itemRemoveBtn);
+  itemBtnsContainer.appendChild(itemDoneBtn);
+  todoItemElement.appendChild(itemBtnsContainer);
+  pageListElement.appendChild(todoItemElement);
+
+  addItemIsDoneListener(itemDoneBtn, todoItem.id, todoItemElement);
+  addRemoveItemListener(itemRemoveBtn, todoItem.id);
+};
 
 let doneListArr = [];
 
@@ -33,11 +89,11 @@ doneListBtn.addEventListener("click", () => {
 const reRenderDoneList = (doneList) => {
   pageListElement.innerHTML = "";
   doneList.forEach((item) => {
-    addDoneItem(item);
+    renderDoneItem(item);
   });
 };
 
-const addDoneItem = (doneitem) => {
+const renderDoneItem = (doneitem) => {
   const todoItemElement = document.createElement("div");
   todoItemElement.className = "todo-item";
 
@@ -60,49 +116,6 @@ const addDoneItem = (doneitem) => {
 
   addItemUnDoneListener(itemUndoneBtn, doneitem.id, todoItemElement);
   addRemoveDoneItemListener(itemRemoveBtn, doneitem.id);
-};
-
-addTodoBtn.addEventListener("click", () => {
-  if (!addTodoInput.value) return;
-  const todoItem = { value: addTodoInput.value, isDone: false, id: Date.now() };
-  todoArr.push(todoItem);
-
-  reRenderToDoList(todoArr);
-
-  addTodoInput.value = "";
-  addTodoBtn.parentElement.removeChild(addTodoBtn);
-});
-
-const reRenderToDoList = (todoList) => {
-  pageListElement.innerHTML = "";
-  todoList.forEach((item) => {
-    addTodoItem(item);
-  });
-};
-
-const addTodoItem = (todoItem) => {
-  const todoItemElement = document.createElement("div");
-  todoItemElement.className = "todo-item";
-
-  const itemBtnsContainer = document.createElement("div");
-  itemBtnsContainer.className = "item-btns-container";
-
-  const itemRemoveBtn = document.createElement("button");
-  itemRemoveBtn.className = "item-remove-btn";
-  itemRemoveBtn.innerText = "remove";
-
-  const itemDoneBtn = document.createElement("button");
-  itemDoneBtn.className = "item-done-btn";
-  itemDoneBtn.innerText = "done";
-
-  todoItemElement.innerText = todoItem.value;
-  itemBtnsContainer.appendChild(itemRemoveBtn);
-  itemBtnsContainer.appendChild(itemDoneBtn);
-  todoItemElement.appendChild(itemBtnsContainer);
-  pageListElement.appendChild(todoItemElement);
-
-  addItemIsDoneListener(itemDoneBtn, todoItem.id, todoItemElement);
-  addRemoveItemListener(itemRemoveBtn, todoItem.id);
 };
 
 const addRemoveItemListener = (element, itemId) => {
@@ -144,12 +157,13 @@ const addItemUnDoneListener = (element, itemId) => {
 };
 
 const switchColorBtn = document.querySelector(".switch-color-btn");
+const helperElement = document.querySelector(".helper");
 let isLightMode = true;
 
 switchColorBtn.addEventListener("click", () => {
   if (!isLightMode) {
     document.body.style.backgroundColor = "initial";
-    pageListElement.style.backgroundColor = "initial";
+    helperElement.style.backgroundColor = "initial";
     pageListElement.style.color = "initial";
     addTodoInput.style.backgroundColor = "initial";
     addTodoInput.parentElement.style.backgroundColor = "initial";
@@ -157,7 +171,7 @@ switchColorBtn.addEventListener("click", () => {
     isLightMode = !isLightMode;
   } else {
     document.body.style.backgroundColor = "black";
-    pageListElement.style.backgroundColor = "#262424";
+    helperElement.style.backgroundColor = "#262424";
     pageListElement.style.color = "rgb(219 216 216)";
     // itemBtnsContainer.style.color = "#cdcdcd";
     addTodoInput.style.backgroundColor = "#262424";
