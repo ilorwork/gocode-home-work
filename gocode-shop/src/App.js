9f4dd2b21dc81ce4dd0041ad768e9057f0ab76be
ilorwork <ilorwork64@gmail.com>
2022-08-20 22:54:38 +0300

    Seperate to files

--- gocode-shop/src/App.js
+++ gocode-shop/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Nav from "./components/Nav";
+import Products from "./components/Products";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="main-container">
+      <Nav />
+      <Products />
+    </div>
+  );
 }
 
 export default App;
