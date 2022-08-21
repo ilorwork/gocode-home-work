412df7efa66f50c02be4a4888416d4f8d8cf2466
ilorwork <ilorwork64@gmail.com>
2022-08-21 03:52:01 +0300

    Remove useless code

--- gocode-shop/src/components/Products.js
+++ gocode-shop/src/components/Products.js
@@ -5,37 +5,15 @@ import "./Products.css";
 
 const Products = () => {
   const [productsArr, setProductsArr] = useState([]);
-  // let productsArr = [];
 
   const fetchProducts = () => {
     fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
       .then((data) => setProductsArr(data));
-
-    debugger;
-    // console.log(productsArr);
-
-    // setProductsArr(data);
-    // return data;
   };
 
-  // const fetchProducts = async () => {
-  //   const response = await fetch("https://fakestoreapi.com/products");
-  //   const data = await response.json();
-
-  //   // console.log(productsArr);
-
-  //   setProductsArr(data);
-  //   // return data;
-  // };
-
-  // Calling here
-  // fetchProducts();
-
   useEffect(() => {
-    // hideLoader();
     fetchProducts();
-    // console.log(productsArr);
   }, []);
 
   const productComponnents = productsArr.map((item) => (
