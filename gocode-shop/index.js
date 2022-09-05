c3356a7548d09a637a5aa0b932297831a82a0e0a
ilorwork <ilorwork64@gmail.com>
2022-09-05 20:26:25 +0300

    Connect to Mongo atlas

--- gocode-shop/index.js
+++ gocode-shop/index.js
@@ -5,7 +5,6 @@ const { productAllowedUpdates } = require("./constants/allowedUpdates");
 const serverResponse = require("./utils/serverResponse");
 const app = express();
 
-// TODO: The .env file is currently empty.
 require("dotenv").config();
 
 app.use(express.json());
@@ -121,11 +120,23 @@ app.get("*", (req, res) => {
   res.sendFile(__dirname + "/client/build/index.html");
 });
 
-mongoose.connect("mongodb://localhost:27017/test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+//mongoose.connect("mongodb://localhost:27017/test", {
+//useNewUrlParser: true,
+//useUnifiedTopology: true,
+//});
 
-app.listen(8000, () => {
-  console.log("Server running at http://127.0.0.1:8000/");
-});
+//app.listen(8000, () => {
+//console.log("Server running at http://127.0.0.1:8000/");
+//});
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME, PORT } = process.env;
+
+mongoose.connect(
+  `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    app.listen(PORT || 8000, () => {
+      console.log("err", err);
+      console.log("Ani maazin!");
+    });
+  }
+);
